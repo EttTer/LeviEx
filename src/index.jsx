6a1f0e915315505c8ed5b18794ebdb16659c578e
EttTer <tereza.ettlerova@gmail.com>
2023-04-08 16:44:41 +0200

    routování

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,37 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import './style.css';
+import { Home } from './components/Home';
+import { Reservation } from './components/Reservation';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
 export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+const ErrorPage = () => {
+    return (
+      <main>
+        <h2>404: Škoda, asi čekáte něco, co tu není </h2>
+        <p>Možná jste se spletli, zkuste to jinak</p>
+      </main>
+    );
+  };
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />, 
+      errorElement: <ErrorPage />,
+    },
+    {path: "/reservation",
+      element: <Reservation />}
+  ]);
+  
+  createRoot(document.querySelector("#app")).render(
+    <RouterProvider router={router} />
+  );
+  
+
+
